ca46f7c5490297a598e6625bf96a8b597678358f
chan <cboswell1024@gmail.com>
2021-08-15 15:26:41 -0400

    all project progress thus far

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: '794245210531-7kf42858aifshcjf2kt5a7h9qd3clv96.apps.googleusercontent.com',
+  googleClientSecret: 'e-0XF-Lb2M_uoDCoAxK8TG1o',
+  mongoURI: 'mongodb+srv://chan:Password1234@ballast.keibv.mongodb.net/ballast?retryWrites=true&w=majority',
+  cookieKey: 'sirmixalotrulez',
+};
